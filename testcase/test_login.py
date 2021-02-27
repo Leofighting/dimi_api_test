cbd7ccd44a3bddb1f5f529f7e05433191c4b0575
unknown <xiaojianwen@nanzhuang.com>
2021-02-27 17:46:52 +0800

    update

--- testcase/test_login.py
+++ testcase/test_login.py
@@ -1,10 +1,14 @@
+import allure
+
 from api.login import Login
 
 
+@allure.feature("登录模块")
 class TestLogin:
     def setup_class(self):
         self.login = Login()
 
+    @allure.story("使用账户密码登录")
     def test_login_with_password(self):
         """
         测试使用用户名密码进行登录
